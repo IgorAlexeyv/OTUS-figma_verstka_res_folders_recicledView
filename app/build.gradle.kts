f6b48b652b929ab2a67f0a71adf9328ff09d3e84
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-12-26 12:34:36 +0300

    Resource folders

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,17 @@ android {
     buildFeatures {
         viewBinding = true
     }
+    sourceSets {
+        getByName("main") {
+            res {
+                srcDirs(
+                    "src/main/res",
+                    "src/main/res/layouts/main",
+                    "src/main/res/layouts/recycler"
+                )
+            }
+        }
+    }
 }
 
 dependencies {
