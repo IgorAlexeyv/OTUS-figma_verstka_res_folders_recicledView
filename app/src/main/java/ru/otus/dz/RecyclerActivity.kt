3598eb67f6d6c77df8d08d036e34c4bbef280890
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-12-27 09:55:49 +0300

    Picture

--- app/src/main/java/ru/otus/dz/RecyclerActivity.kt
+++ app/src/main/java/ru/otus/dz/RecyclerActivity.kt
@@ -3,9 +3,6 @@ package ru.otus.dz
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import ru.otus.dz.databinding.ActivityMainBinding
 import ru.otus.dz.databinding.ActivityRecyclerBinding
 
 class RecyclerActivity : AppCompatActivity() {
